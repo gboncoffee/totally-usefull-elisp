8fc4bf8394a1b84768413876bd153eab8700b3ae
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-03-07 22:03:30 -0300

    Added cowsay

--- usefull.el
+++ usefull.el
@@ -10,3 +10,16 @@
 	     (format "%s, at least that's what your computer said me." (getenv "USER"))
 	   (format "%s, at least that's what you said me." user-full-name))))
     (message "You're %s" write-after)))
+
+(defun cowsay ()
+  "The world-famous Cow says. Uses the variable
+   cowsay-command. Defaults to 'cowsay $(fortune)'"
+  (interactive)
+    (switch-to-buffer "*cowsay*")
+    (read-only-mode -1)
+    (erase-buffer)
+    (insert (shell-command-to-string
+	     (if (and (boundp 'cowsay-command) (not (null cowsay-command)))
+		 cowsay-command
+	       "cowsay $(fortune)")))
+    (read-only-mode))
